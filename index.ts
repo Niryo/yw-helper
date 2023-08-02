af16f971f71ce0c1a4d9f9b834b6f867dc3d0071
Nir <niryo@wix.com>
2023-08-02 18:50:14 +0300

    added last workspace option

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import inquirerPrompt from 'inquirer-autocomplete-prompt';
 import chalk from 'chalk';
 import {version} from './version.js';
 import path from 'path';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 inquirer.registerPrompt('autocomplete', inquirerPrompt);
@@ -19,15 +19,23 @@ yarnWorkspaceRun
   .allowUnknownOption(true)
   .argument('[workspaceName]', 'The name of the workspace')
   .argument('[command...]', 'The command to run')
-  .option('-l, --last-command', 'Run the last command that was run with this tool')
+  .option('-r, --re-run', 'Run again the last command')
+  .option('-w, --reuse-workspace', 'Use the same workspace as last time')
   .action(async (workspaceNameInputOrCommand: string, commandInput: string[]) => {
-    if(yarnWorkspaceRun.opts().lastCommand) {
-      const lastCommand = fs.readFileSync(`${__dirname}/lastCommand.txt`, 'utf-8').trim();
-      console.log(chalk.green(`Running last command: ${lastCommand}`));
-      execSync(lastCommand, {stdio: 'inherit'});
-      return;
-    }
     verifyYarn();
+
+    if (yarnWorkspaceRun.opts().reRun) {
+      const lastCommandPath = `${__dirname}/lastWorkspace.txt`;
+      if (fs.existsSync(lastCommandPath)) {
+        const lastCommand = fs.readFileSync(lastCommandPath, 'utf-8').trim();
+        console.log(chalk.green(`Running last command: ${lastCommand}`));
+        execSync(lastCommand, {stdio: 'inherit'});
+        return;
+      } else {
+        console.log(chalk.yellow('Could not find last command'));
+      }
+    }
+
     const allWorkspaces: Record<string, string> =
       listWorkspaces()
         .split('\n')
@@ -39,8 +47,13 @@ yarnWorkspaceRun
         }, {});
     const allWorkspacesNames = Object.keys(allWorkspaces);
     const isSingleWorkspace = allWorkspacesNames.length === 1;
+
     let workspaceName: string;
-    if(isSingleWorkspace) {
+    const lastWorkspacePath = `${__dirname}/lastWorkspace.txt`;
+    if (yarnWorkspaceRun.opts().reuseWorkspace && fs.existsSync(lastWorkspacePath)) {
+        workspaceName = fs.readFileSync(lastWorkspacePath, 'utf-8').trim();
+        console.log(chalk.green(`Using last workspace: ${workspaceName}`));
+    } else if (isSingleWorkspace) {
       workspaceName = allWorkspacesNames[0];
       // if there is only one workspace, we assume workspaceNameInputOrCommand is actually a command so we add it to the commandInput array
       workspaceNameInputOrCommand && commandInput.unshift(workspaceNameInputOrCommand);
@@ -50,11 +63,11 @@ yarnWorkspaceRun
       workspaceName = await getWorkspaceName(workspaceNameInput, allWorkspacesNames);
     }
     let script: string;
-    if(commandInput.length > 0) {
+    if (commandInput.length > 0) {
       script = commandInput.join(' ');
-    } else if(allWorkspacesNames.length === 1 && workspaceNameInputOrCommand) {
+    } else if (allWorkspacesNames.length === 1 && workspaceNameInputOrCommand) {
       script = workspaceNameInputOrCommand.concat(...commandInput); //if there is only one workspace, we assume workspaceNameInputOrCommand is actually a command
-    }  else {
+    } else {
       script = await askForScriptToRun(allWorkspaces[workspaceName]);
     }
     const commandToRun = script !== 'run' ? script : await askForCustomCommandToRun();
@@ -62,6 +75,7 @@ yarnWorkspaceRun
     const finalCommand = `yarn workspace ${workspaceName} ${commandToRun}`;
     console.log(chalk.green(`Running: ${finalCommand}`));
     fs.writeFileSync(`${__dirname}/lastCommand.txt`, finalCommand, 'utf-8');
+    fs.writeFileSync(`${__dirname}/lastWorkspace.txt`, workspaceName, 'utf-8');
     execSync(finalCommand, {stdio: 'inherit'});
   });
 yarnWorkspaceRun.parse(process.argv);
